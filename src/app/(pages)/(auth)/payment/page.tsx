3f5d9856e0552a11be805e26b9a70077aca37e51
@azher <azher@renesistech.com>
2023-12-24 15:18:50 +0500

    stripe integration complete

--- src/app/(pages)/(auth)/payment/page.tsx
+++ src/app/(pages)/(auth)/payment/page.tsx
@@ -1,13 +1,60 @@
 'use client';
-import NavBar from '@/app/(pages)/(auth)/authNavbar';
-import Progessbar from '@/app/component/progressBar';
-import { secondaryHeading } from '@/globals/tailwindvariables';
+import React , {useState} from 'react';
+import { loadStripe } from "@stripe/stripe-js"; 
 import Image from 'next/image';
+
+// module imports
+import { secondaryHeading } from '@/globals/tailwindvariables';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import Progessbar from '@/app/component/progressBar';
+import NavBar from '@/app/(pages)/(auth)/authNavbar';
 
 const Payment = () => {
   const router = useRouter();
+
+
+  const [product, setProduct] = useState({ 
+    name: "Go FullStack with KnowledgeHut", 
+    price: 1000, 
+    productOwner: "KnowledgeHut", 
+    description: 
+      "This beginner-friendly Full-Stack Web Development Course is offered online in blended learning mode, and also in an on-demand self-paced format.", 
+    quantity: 1, 
+  }); 
+
+
+
+  const makePayment = async () => { 
+    const stripe : any = await loadStripe("pk_test_51JMvNpAIsBBwEZbsJuMCirc88j6z96mKj9ycl497ozu9ljMEIHFWBc7cIVVqKHdY34B8q3u4mn0jeXqM1rvP3szs00PzPtx0ZX"); 
+    const body = { product }; 
+    const headers = { 
+      "Content-Type": "application/json", 
+    }; 
+ 
+    const response = await fetch( 
+      "http://localhost:4000/api/auth/create-checkout-session", 
+      { 
+        method: "POST", 
+        headers: headers, 
+        body: JSON.stringify(body), 
+      } 
+    ); 
+
+ 
+    const session = await response.json(); 
+    
+ 
+    const result = stripe.redirectToCheckout({ 
+      sessionId: session.data.id, 
+    });
+    
+ 
+    if (result.error) { 
+      console.log(result.error); 
+    } 
+  }; 
+
+  
   return (
     <>
       <NavBar />
@@ -18,14 +65,8 @@ const Payment = () => {
           {/* methods */}
           <div className="flex justify-between items-center">
             <div
-              className="h-52
-         grid place-items-center
-          w-80 border-2 
-          my-6 gap-10
-          border-doveGray rounded-s
-          cursor-pointer
-          "
-              onClick={() => router.push('/congratulation')}
+              className="h-52 grid place-items-center w-80 border-2  my-6 gap-10 border-doveGray rounded-s cursor-pointer"
+              onClick={makePayment}
             >
               <Image
                 src={'/stripe.svg'}
@@ -35,11 +76,7 @@ const Payment = () => {
               />
             </div>
             <div
-              className="h-52
-         grid place-items-center
-          w-80 border-2 
-          border-doveGray rounded-s"
-            >
+              className="h-52 grid place-items-center w-80 border-2  border-doveGray rounded-s"  onClick={() => router.push('/congratulation')}>
               <Image
                 src={'/paypal.svg'}
                 alt={'paypal icon'}
