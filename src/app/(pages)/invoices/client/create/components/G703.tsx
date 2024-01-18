05e7e9be677b2a34f467b75843c39678302be79d
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-01-18 20:08:26 +0500

    g703 title

--- src/app/(pages)/invoices/client/create/components/G703.tsx
+++ src/app/(pages)/invoices/client/create/components/G703.tsx
@@ -1,6 +1,26 @@
+
+import PrimaryHeading from "@/app/component/headings/primary";
+import QuaternaryHeading from "@/app/component/headings/quaternary";
+import { DownOutlined } from "@ant-design/icons";
+
 export function G703Component() {
 
-    return <div>
-        <h1>G703 Component</h1>
-    </div>
+    return <section>
+        <div className="flex justify-between items-center">
+            <div>
+                <QuaternaryHeading title="AIA Document G703, - 1992" />
+                <PrimaryHeading title="Continuation Sheet"
+                    className="font-normal"
+                />
+            </div>
+            <div>
+                <div
+                    className={"flex h-10 w-full items-center justify-between rounded-md border border-input  px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}
+                >
+                    <span>Select Previous Phase</span>
+                    <DownOutlined />
+                </div>
+            </div>
+        </div>
+    </section>
 }
