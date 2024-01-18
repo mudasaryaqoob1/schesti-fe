2c61d092f666aac3d051605891fef47a4444421a
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-01-18 19:24:20 +0500

    display invoice name

--- src/app/(pages)/invoices/client/create/page.tsx
+++ src/app/(pages)/invoices/client/create/page.tsx
@@ -5,9 +5,13 @@ import { useSelector } from "react-redux";
 
 import { HttpService } from "@/app/services/base.service";
 import { selectToken } from "@/redux/authSlices/auth.selector";
+import { useSearchParams } from "next/navigation";
+import TertiaryHeading from "@/app/component/headings/tertiary";
 
 export default function CreateClientInvoicePage() {
   const token = useSelector(selectToken);
+  const searchParams = useSearchParams()
+  const invoiceName = searchParams.get("invoiceName");
 
 
   useLayoutEffect(() => {
@@ -15,9 +19,23 @@ export default function CreateClientInvoicePage() {
       HttpService.setToken(token);
     }
   }, [token]);
+
+
   return (
-    <div>
-      <h1>Create Client Invoice Page</h1>
-    </div>
+    <section className="mx-16 my-2">
+      <div className="p-5 shadow-md rounded-lg border border-silverGray  bg-white">
+        <div className="flex space-x-3">
+          <TertiaryHeading title="Involve name:"
+            className="font-medium"
+          />
+          <TertiaryHeading title={`${invoiceName}`} />
+        </div>
+      </div>
+
+
+      <div className="p-5 shadow-md rounded-lg border border-silverGray  bg-white">
+
+      </div>
+    </section>
   )
 }
