d1cfe04c510a0a524845df91ca5c53c9e08a4949
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-01-18 19:29:29 +0500

    tabs for g703 and g702

--- src/app/(pages)/invoices/client/create/page.tsx
+++ src/app/(pages)/invoices/client/create/page.tsx
@@ -1,18 +1,24 @@
 'use client';
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { ConfigProvider, Tabs } from "antd";
+import { useSearchParams } from "next/navigation";
 
 
 import { HttpService } from "@/app/services/base.service";
 import { selectToken } from "@/redux/authSlices/auth.selector";
-import { useSearchParams } from "next/navigation";
 import TertiaryHeading from "@/app/component/headings/tertiary";
+import QuaternaryHeading from "@/app/component/headings/quaternary";
+
+
+const G703_KEY = "G703";
+const G702_KEY = "G702";
 
 export default function CreateClientInvoicePage() {
   const token = useSelector(selectToken);
   const searchParams = useSearchParams()
   const invoiceName = searchParams.get("invoiceName");
-
+  const [tab, setTab] = useState(G703_KEY);
 
   useLayoutEffect(() => {
     if (token) {
@@ -33,8 +39,38 @@ export default function CreateClientInvoicePage() {
       </div>
 
 
-      <div className="p-5 shadow-md rounded-lg border border-silverGray  bg-white">
-
+      <div className="px-4 py-2 my-5 shadow-md rounded-lg border border-silverGray  bg-white">
+        <ConfigProvider
+          theme={{
+            components: {
+              Tabs: {
+                inkBarColor: '#8449EB',
+              },
+            },
+          }}
+        >
+          <Tabs
+            defaultActiveKey={G703_KEY}
+            destroyInactiveTabPane
+            onChange={(type) => {
+              setTab(type);
+            }}
+            items={[G703_KEY, G702_KEY].map((type) => {
+              return {
+                key: type,
+                label: (
+                  <QuaternaryHeading
+                    title={type}
+                    className="text-RoyalPurple"
+                  />
+                ),
+                tabKey: type,
+                children:
+                  tab === G703_KEY ? "G703" : "G702",
+              };
+            })}
+          />
+        </ConfigProvider>
       </div>
     </section>
   )
