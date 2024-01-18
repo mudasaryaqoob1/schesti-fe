78b6941fa36a304e3f280d10b797224b846b1a00
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-01-18 21:05:33 +0500

    basic info collecting inputs

--- src/app/(pages)/invoices/client/create/components/G703.tsx
+++ src/app/(pages)/invoices/client/create/components/G703.tsx
@@ -1,31 +1,16 @@
-import { DownOutlined } from "@ant-design/icons";
-import { Divider, Dropdown, type MenuProps } from "antd";
+import { Divider, Select } from "antd";
 
 import CustomButton from "@/app/component/customButton/button";
 import WhiteButton from "@/app/component/customButton/white";
 import PrimaryHeading from "@/app/component/headings/primary";
 import QuaternaryHeading from "@/app/component/headings/quaternary";
-import QuinaryHeading from "@/app/component/headings/quinary";
-import SenaryHeading from "@/app/component/headings/senaryHeading";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
 
 export function G703Component() {
 
-    const items: MenuProps['items'] = [
-        {
-            key: "phase1",
-            label: <SenaryHeading title="Phase 1" />
-        },
-        {
-            key: "phase2",
-            label: <SenaryHeading title="Phase 2" />
-        },
 
-        {
-            key: "phase3",
-            label: <SenaryHeading title="Phase 3" />
-        },
-
-    ];
 
     return <section>
         <div className="flex justify-between items-center">
@@ -36,32 +21,76 @@ export function G703Component() {
                 />
             </div>
             <div>
-                <Dropdown
-                    menu={{ items }}
-                    placement="bottom"
-                >
-                    <div
-                        className={"flex cursor-pointer h-10 w-full items-center justify-between rounded-md border border-input space-x-5 px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}
-                    >
-                        <QuinaryHeading
-                            title="Select Previous Phase"
-                            className="text-gray-400"
-                        />
-                        <DownOutlined className="text-gray-400" />
-                    </div>
-                </Dropdown>
+                <Select
+                    placeholder="Select Previous Phase"
+                    options={[
+                        { value: 'phase1', label: 'Phase 1' },
+                        { value: 'phase2', label: 'Phase 2' },
+                        { value: 'phase3', label: 'Phase 3' },
+                    ]}
+                    style={{ width: 250 }}
+                    size="large"
+                />
             </div>
 
         </div>
-        <Divider />
+        <Divider className="!mt-6 !m-0" />
 
-        <div className="flex">
+        <div className="flex justify-between">
             <div>
+                <QuaternaryHeading title={`AIA Document G702, APPLICATION AND CERTIFICATION FOR PAYMENT, containing
+                                        Contractor's signed certification is attached.
 
+                                        In tabulations below, amounts are stated to the nearest dollar.
+                                        
+                                        Use Column I on Contracts where variable retainage for line items may apply.`}
+
+                    className="max-w-2xl" />
             </div>
 
-            <div></div>
-        </div>
+            <div className="flex flex-col p-4 space-y-3 bg-white flex-1">
+                <div className="flex self-end space-x-3 items-center">
+                    <label className="text-right text-graphiteGray font-normal" htmlFor="application-date">
+                        APPLICATION NO:
+                    </label>
+                    <input
+                        id="application-date"
+                        className="px-2 py-1 border border-gray-300"
+                        type="text"
+                    />
+                </div>
+                <div className="flex self-end space-x-3 items-center">
+                    <label className="text-right text-graphiteGray font-normal" htmlFor="application-date">
+                        APPLICATION DATE:
+                    </label>
+                    <input
+                        id="application-date"
+                        className="px-2 py-1 border border-gray-300"
+                        type="text"
+                    />
+                </div>
+                <div className="flex self-end space-x-3 items-center">
+                    <label className="text-right text-graphiteGray font-normal" htmlFor="application-date">
+                        PERIOD TO:
+                    </label>
+                    <input
+                        id="application-date"
+                        className="px-2 py-1 border border-gray-300"
+                        type="text"
+                    />
+                </div>
+                <div className="flex self-end space-x-3 items-center">
+                    <label className="text-right text-graphiteGray font-normal" htmlFor="application-date">
+                        PROJECT NO:
+                    </label>
+                    <input
+                        id="application-date"
+                        className="px-2 py-1 border border-gray-300"
+                        type="text"
+                    />
+                </div>
+            </div>
+        </div >
 
         <div className="flex justify-end space-x-4">
             <WhiteButton
@@ -75,5 +104,5 @@ export function G703Component() {
             />
 
         </div>
-    </section>
+    </section >
 }
