5e718b47c652373f75608c9d80d481111b7feb3d
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-01-18 20:16:26 +0500

    dropdown for phases

--- src/app/(pages)/invoices/client/create/components/G703.tsx
+++ src/app/(pages)/invoices/client/create/components/G703.tsx
@@ -1,9 +1,29 @@
 
 import PrimaryHeading from "@/app/component/headings/primary";
 import QuaternaryHeading from "@/app/component/headings/quaternary";
+import QuinaryHeading from "@/app/component/headings/quinary";
+import SenaryHeading from "@/app/component/headings/senaryHeading";
 import { DownOutlined } from "@ant-design/icons";
+import { Dropdown, type MenuProps } from "antd";
 
 export function G703Component() {
+
+    const items: MenuProps['items'] = [
+        {
+            key: "phase1",
+            label: <SenaryHeading title="Phase 1" />
+        },
+        {
+            key: "phase2",
+            label: <SenaryHeading title="Phase 2" />
+        },
+
+        {
+            key: "phase3",
+            label: <SenaryHeading title="Phase 3" />
+        },
+
+    ];
 
     return <section>
         <div className="flex justify-between items-center">
@@ -14,12 +34,19 @@ export function G703Component() {
                 />
             </div>
             <div>
-                <div
-                    className={"flex h-10 w-full items-center justify-between rounded-md border border-input  px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}
+                <Dropdown
+                    menu={{ items }}
+                    placement="bottom"
                 >
-                    <span>Select Previous Phase</span>
-                    <DownOutlined />
-                </div>
+                    <div
+                        className={"flex cursor-pointer h-10 w-full items-center justify-between rounded-md border border-input space-x-5 px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}
+                    >
+                        <QuinaryHeading
+                            title="Select Previous Phase"
+                        />
+                        <DownOutlined />
+                    </div>
+                </Dropdown>
             </div>
         </div>
     </section>
