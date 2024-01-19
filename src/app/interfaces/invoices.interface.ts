f0fbf6ef7b7a90508085990e3d1f2aed81db8152
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-01-19 21:30:24 +0500

    invoice type

---
+++ src/app/interfaces/invoices.interface.ts
@@ -0,0 +1,33 @@
+export interface ISupportTicket {
+    invoice: Invoice
+}
+
+interface Invoice {
+    subContractorFirstName: string
+    subContractorLastName: string
+    subContractorPhoneNumber: string
+    subContractorEmail: string
+    subContractorAddress: string
+    invoiceNumber: string
+    projectName: string
+    applicationNumber: string
+    invoiceSubject: string
+    issueDate: string
+    dueDate: string
+    invoiceItems: InvoiceItem[]
+    discount: string
+    taxes: string
+    profitAndOverhead: string
+    totalPayable: number
+    associatedComapny: string
+    _id: string
+    __v: number
+}
+
+interface InvoiceItem {
+    description: string
+    quantity: number
+    unitCost: number
+    total: number
+    _id: string
+}
