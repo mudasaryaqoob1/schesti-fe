7732b280e603324eae4f1d3ed6f706cb3100fee8
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-02-05 19:52:39 +0500

    find the meeting

--- src/app/(pages)/meeting/[link]/page.tsx
+++ src/app/(pages)/meeting/[link]/page.tsx
@@ -1,20 +1,80 @@
 'use client';
+import CustomButton from '@/app/component/customButton/button';
+import Description from '@/app/component/description';
+import SecondaryHeading from '@/app/component/headings/Secondary';
 import { IMeeting } from '@/app/interfaces/meeting.type';
+import { HttpService } from '@/app/services/base.service';
+import { selectToken } from '@/redux/authSlices/auth.selector';
+import { selectMeetings } from '@/redux/meeting/meeting.slice';
+import { fetchMeetings } from '@/redux/meeting/meeting.thunk';
+import { AppDispatch, RootState } from '@/redux/store';
 import { JaaSMeeting } from '@jitsi/react-sdk';
+import { Skeleton } from 'antd';
+import Image from 'next/image';
 import { useParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function JoinMeeting() {
   const { link: roomName } = useParams();
   const router = useRouter();
-  const [meeting, setMeeting] = useState<IMeeting | null>(null);
+  const token = useSelector(selectToken);
+  const meetings = useSelector(selectMeetings);
+  const meetingLoading = useSelector((state: RootState) => state.meetings.loading);
+  const dispatch = useDispatch<AppDispatch>();
 
-  console.log(roomName);
+  useLayoutEffect(() => {
+    if (token) {
+      HttpService.setToken(token);
+    }
+  }, [token]);
+
+  const fetchMeetingsCB = useCallback(async () => {
+    await dispatch(fetchMeetings({}));
+  }, [dispatch]);
+
+  useEffect(() => {
+    fetchMeetingsCB();
+  }, [fetchMeetingsCB]);
 
   if (!roomName) {
     router.back();
     return <div>Loading...</div>;
+  }
+  if (meetingLoading) {
+    return <Skeleton active className="mt-6" />;
+  }
+
+  const meeting = meetings.find((m: IMeeting) => m.link === roomName);
+  if (!meeting) {
+    return <section className="mt-6 mx-4 rounded-xl h-[calc(100vh-200px)] grid items-center border border-solid border-silverGray shadow-secondaryTwist">
+      <div className="grid place-items-center">
+        <div className="max-w-[500px] flex flex-col items-center p-4">
+          <div className="bg-lightGray p-12 rounded-full">
+            <Image
+              src={'/purple-calendar.svg'}
+              alt="create request icon"
+              width={100}
+              height={100}
+            />
+          </div>
+          <SecondaryHeading
+            title="No meeting found"
+            className="text-obsidianBlack2 mt-8"
+          />
+          <Description
+            title="There is no scheduled meeting. Initiate one by using the Jitsi integration."
+            className="text-steelGray text-center font-normal"
+          />
+          <CustomButton
+            className="mt-7"
+            text={'Go Back'}
+          />
+        </div>
+      </div>
+    </section>
   }
 
   return (
