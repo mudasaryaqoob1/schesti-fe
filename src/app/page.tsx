cde3954538f6620c0e4a42476c0633d90f46d418
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-02-10 23:23:35 +0500

    landing page init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Dashboard from './(pages)/dashboard/page';
+import { LandingNavbar } from "./component/navbar/LandingNavbar";
 
 export default function Home() {
   return (
-    <main>
-      <Dashboard />
+    <main style={{
+      background: "linear-gradient(180deg, #8449EB 0%, #6A56F6 100%)"
+    }} className="h-[760px]">
+      <LandingNavbar />
     </main>
   );
 }
