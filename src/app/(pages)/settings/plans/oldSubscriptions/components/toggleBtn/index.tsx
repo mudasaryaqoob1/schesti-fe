c7976387c7f597365cd75ff802b6fc93a291dcc2
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-02-13 03:15:58 +0500

    fix ui issues

--- src/app/(pages)/settings/plans/oldSubscriptions/components/toggleBtn/index.tsx
+++ src/app/(pages)/settings/plans/oldSubscriptions/components/toggleBtn/index.tsx
@@ -12,7 +12,7 @@ const Index: FC<ToggleBtnProps> = ({ planType, onChange }) => {
     <div className=" min-w-[350px] h-14">
       <label
         htmlFor="Toggle3"
-        className="inline-flex gap-4  bg-cloudWhite items-center justify-center cursor-pointer dark:text-gray-800 rounded-xl w-full h-full p-1"
+        className="inline-flex gap-4  bg-cloudWhite items-center justify-center cursor-pointer dark:text-gray-800 rounded-full w-full h-full p-1"
       >
         <input
           id="Toggle3"
@@ -22,18 +22,16 @@ const Index: FC<ToggleBtnProps> = ({ planType, onChange }) => {
           checked={planType === 'Individual' ? true : false}
         />
         <span
-          className={`px-4 py-2 rounded-l-md dark:bg-lavenderPurple peer-checked:dark:bg-cloudWhite flex-1 text-center
-        dark:text-nastyWhite peer-checked:dark:text-ebonyGray h-full
+          className={`px-4 py-2 rounded-full ${planType === "Individual" ? "bg-lavenderPurple text-nastyWhite" : ""} bg-cloudWhite flex-1 text-center h-full
         grid place-items-center ${quaternaryHeading} font-medium 
         `}
         >
           Individual Plan
         </span>
         <span
-          className={`px-4 py-2 rounded-r-md dark:bg-cloudWhite peer-checked:dark:bg-lavenderPurple flex-1 text-center
-        peer-checked:dark:text-nastyWhite  h-full grid place-items-center
-        ${quaternaryHeading} font-medium 
-        `}
+          className={`px-4 py-2 rounded-full ${planType === "Enterprise" ? "bg-lavenderPurple text-nastyWhite" : ""} bg-cloudWhite flex-1 text-center h-full
+grid place-items-center ${quaternaryHeading} font-medium 
+`}
         >
           Enterprise Plan
         </span>
