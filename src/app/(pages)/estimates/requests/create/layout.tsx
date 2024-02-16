c6fb74c816082b214aa1e7b0ee0605f30fab219b
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-02-17 00:12:30 +0500

    Schesti||Estimate||Esitmate Request|| Add Request - BUG: Change the Title text of the page .

---
+++ src/app/(pages)/estimates/requests/create/layout.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Schesti - Create Estimate',
+    description: 'Schesti - Create Estimate',
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return children
+}
