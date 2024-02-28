9de7aa75a3b15c90ce58cb41d66950eb9862353f
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-02-28 19:15:32 +0500

    display ads

--- src/app/(pages)/dashboard/components/AdsManagement.tsx
+++ src/app/(pages)/dashboard/components/AdsManagement.tsx
@@ -1,42 +1,49 @@
 import CustomButton from '@/app/component/customButton/white';
 import Description from '@/app/component/description';
-import { Carousel } from 'antd';
+import { adService } from '@/app/services/ad-management.service';
+import { Carousel, Skeleton } from 'antd';
 import Image from 'next/image';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-const data = [
-  {
-    id: 1,
-    clientName: 'AdsManagement 1',
-    startDate: '2022-01-01',
-    expiryDate: '2022-01-02',
-    picture:
-      'https://s3-alpha-sig.figma.com/img/fa49/6391/2bed79afc1a8eba2ddb92945d7428145?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=na6IENwlBdeoydn3crQ7X1ldHZAiUQ69S4NDrxsAqo3i2cu~FVh2A0C2VN8-tYrkDnYBPQTzNWe1H6aEWdgDSXiuIRmYuZPVWZNczQJutV8IWT5pEto5QAKeZlnefEKBhDZ6DM5jO7vtyqUELDjgSwiXJy2g8Mg308zOSZ8putDdaVxsT5adcZsEzGQ9j-MUjnY2u8-CHwlmQo4geAFXJlaJtAkVTX6DlFiGWIalnleGC2CkdDPJFH5FhYLt04wlvbCEEtItactQmT2JKQ~NCfuFgWDX-TXZAcjYUhTLGZqRg-ZrAvl81vXeZwegPZz9My9yCMmdj-n1~j5KV1Lcig__',
-  },
-  {
-    id: 2,
-    clientName: 'AdsManagement 2',
-    startDate: '2022-01-01',
-    expiryDate: '2022-01-02',
-    picture:
-      'https://s3-alpha-sig.figma.com/img/fa49/6391/2bed79afc1a8eba2ddb92945d7428145?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=na6IENwlBdeoydn3crQ7X1ldHZAiUQ69S4NDrxsAqo3i2cu~FVh2A0C2VN8-tYrkDnYBPQTzNWe1H6aEWdgDSXiuIRmYuZPVWZNczQJutV8IWT5pEto5QAKeZlnefEKBhDZ6DM5jO7vtyqUELDjgSwiXJy2g8Mg308zOSZ8putDdaVxsT5adcZsEzGQ9j-MUjnY2u8-CHwlmQo4geAFXJlaJtAkVTX6DlFiGWIalnleGC2CkdDPJFH5FhYLt04wlvbCEEtItactQmT2JKQ~NCfuFgWDX-TXZAcjYUhTLGZqRg-ZrAvl81vXeZwegPZz9My9yCMmdj-n1~j5KV1Lcig__',
-  },
-  {
-    id: 3,
-    clientName: 'AdsManagement 3',
-    startDate: '2022-01-01',
-    expiryDate: '2022-01-02',
-    picture:
-      'https://s3-alpha-sig.figma.com/img/fa49/6391/2bed79afc1a8eba2ddb92945d7428145?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=na6IENwlBdeoydn3crQ7X1ldHZAiUQ69S4NDrxsAqo3i2cu~FVh2A0C2VN8-tYrkDnYBPQTzNWe1H6aEWdgDSXiuIRmYuZPVWZNczQJutV8IWT5pEto5QAKeZlnefEKBhDZ6DM5jO7vtyqUELDjgSwiXJy2g8Mg308zOSZ8putDdaVxsT5adcZsEzGQ9j-MUjnY2u8-CHwlmQo4geAFXJlaJtAkVTX6DlFiGWIalnleGC2CkdDPJFH5FhYLt04wlvbCEEtItactQmT2JKQ~NCfuFgWDX-TXZAcjYUhTLGZqRg-ZrAvl81vXeZwegPZz9My9yCMmdj-n1~j5KV1Lcig__',
-  },
-];
 export function AdsManagement() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const adsQuery = useQuery(['ads'], () => {
+    return adService.httpGetAllAds();
+  });
+
+  const handleSlideChange = (current: number) => {
+    setCurrentSlide(current);
+  };
+
+  const calculateAutoplaySpeed = (duration?: number) => {
+    if (!duration) {
+      return;
+    }
+    // Convert duration to milliseconds (assuming duration is in seconds)
+    return duration * 1000;
+  };
+
+  if (adsQuery.isLoading) {
+    return <Skeleton />
+  }
+
+
+
   return (
-    <Carousel effect="fade" style={{ height: 250, width: '100%' }}>
-      {data.map((item) => {
+    <Carousel
+      autoplay={true}
+      autoplaySpeed={calculateAutoplaySpeed(adsQuery.data?.data!.ads[currentSlide]?.duration)}
+      afterChange={handleSlideChange}
+      effect="fade" style={{ height: 250, width: '100%' }}
+      dots={false}
+    >
+      {adsQuery.data?.data!.ads.map((item) => {
         return (
           <div
             className="w-full h-56 my-6 shadow-md rounded-md bg-gradient-to-r from-[#7A4FF0] to-[#B29BFF] px-10 pt-4"
-            key={item.id}
+            key={item._id}
           >
             <div className="grid grid-cols-12 gap-8">
               <div className="space-y-2 col-span-3 flex flex-col items-center">
@@ -72,7 +79,7 @@ export function AdsManagement() {
               <div className="col-span-3 flex justify-end">
                 <Image
                   alt="AdsManagement"
-                  src={item.picture}
+                  src={item.imageURL}
                   width={250}
                   height={200}
                   className="rounded-md"
