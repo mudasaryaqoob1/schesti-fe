21f556a25342fc869d6c83762232252f42b40517
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-02-28 23:37:54 +0500

    image carousl

--- src/app/(pages)/dashboard/components/AdsManagement.tsx
+++ src/app/(pages)/dashboard/components/AdsManagement.tsx
@@ -1,63 +1,29 @@
 import { adService } from '@/app/services/ad-management.service';
-import { Carousel, Skeleton } from 'antd';
+import { Skeleton } from 'antd';
 import moment from 'moment';
-import { useState } from 'react';
 import { useQuery } from 'react-query';
+import { AdsCarousel } from './AdsCarousel';
 
 export function AdsManagement() {
-  const [currentSlide, setCurrentSlide] = useState(0);
 
   const adsQuery = useQuery(['ads'], () => {
     return adService.httpGetAllAds();
   });
 
-  const handleSlideChange = (current: number) => {
-    setCurrentSlide(current);
-  };
-
-  const calculateAutoplaySpeed = (duration?: number) => {
-    if (!duration) {
-      return;
-    }
-    // Convert duration to milliseconds (assuming duration is in seconds)
-    return duration * 1000;
-  };
 
   if (adsQuery.isLoading) {
-    return <Skeleton />
+    return <Skeleton />;
   }
 
-  if (!adsQuery.data?.data) {
+  if (adsQuery.isError || !adsQuery.data?.data?.ads || adsQuery.data?.data?.ads.length === 0) {
     return null;
   }
 
-  // filter ads that are between startDate and endDate using moment.js
   const filteredAds = adsQuery.data.data.ads.filter(ad => {
     const startDate = moment(ad.startDate);
     const expiryDate = moment(ad.expiryDate);
     const current = moment();
     return current.isBetween(startDate, expiryDate);
   });
-
-  return (
-    <Carousel
-      autoplay={true}
-      autoplaySpeed={calculateAutoplaySpeed(filteredAds[currentSlide]?.duration)}
-      afterChange={handleSlideChange}
-      effect="fade" style={{ height: 250, width: '100%' }}
-    // dots={false}
-    >
-      {adsQuery.data?.data!.ads.map((item) => {
-        return (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            alt="AdsManagement"
-            src={item.imageURL}
-            className="rounded-md w-full h-56 shadow-md  border"
-            key={item._id}
-          />
-        );
-      })}
-    </Carousel>
-  );
+  return <AdsCarousel ads={filteredAds} />
 }
