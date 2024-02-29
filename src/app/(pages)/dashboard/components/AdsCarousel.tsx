5cfabf34c8d3a26f9f0d5a5125e0b01495daeb79
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-02-29 17:52:30 +0500

    display 2nd ad in advance

--- src/app/(pages)/dashboard/components/AdsCarousel.tsx
+++ src/app/(pages)/dashboard/components/AdsCarousel.tsx
@@ -20,14 +20,28 @@ export function AdsCarousel({ ads }: Props) {
   }, [currentAdIndex, ads]);
 
   return (
-    <div>
-      {ads[currentAdIndex] && (
-        <img
-          src={ads[currentAdIndex].imageURL}
-          alt={`Ad ${currentAdIndex + 1}`}
-          style={{ width: '100%', height: 200, objectFit: 'cover' }}
-          className="rounded-md shadow-md"
-        />
+    <div className="grid grid-cols-12 gap-8">
+      <div className={`col-span-${ads.length === 1 ? '12' : '8'}`}>
+        {ads[currentAdIndex] && (
+          <img
+            src={ads[currentAdIndex].imageURL}
+            alt={`Ad ${currentAdIndex + 1}`}
+            style={{ width: '100%', height: 200, objectFit: 'cover' }}
+            className="rounded-md shadow-md"
+          />
+        )}
+      </div>
+      {ads.length > 1 && (
+        <div className="col-span-4 hidden md:block">
+          {ads[(currentAdIndex + 1) % ads.length] && (
+            <img
+              src={ads[(currentAdIndex + 1) % ads.length].imageURL}
+              alt={`Ad ${currentAdIndex + 2}`}
+              style={{ width: '100%', height: 200, objectFit: 'cover' }}
+              className="rounded-md shadow-md"
+            />
+          )}
+        </div>
       )}
     </div>
   );
