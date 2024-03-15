9b768bf26d653998d9fd437509d8b96e53a8c406
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-03-16 00:01:32 +0500

    dynamic navigation

--- src/app/component/tabs/index.tsx
+++ src/app/component/tabs/index.tsx
@@ -3,25 +3,25 @@
 import { quaternaryHeading } from '@/globals/tailwindvariables';
 import { useRouter } from 'next/navigation';
 import { Dropdown, Space } from 'antd';
-import type { MenuProps } from 'antd';
+// import type { MenuProps } from 'antd';
 import clsx from 'clsx';
 import { usePathname } from 'next/navigation';
 import { twMerge } from 'tailwind-merge';
 import tabsStyle from './tabs.module.css';
 import { DownOutlined } from '@ant-design/icons';
 import Link from 'next/link';
+import { planFeatureOptions } from '@/app/utils/plans.utils';
+// const items: MenuProps['items'] = [
+//   {
+//     key: '1',
+//     label: <Link href="/estimates">Estimates Requests</Link>,
+//   },
 
-const items: MenuProps['items'] = [
-  {
-    key: '1',
-    label: <Link href="/estimates">Estimates Requests</Link>,
-  },
-
-  {
-    key: '2',
-    label: <Link href="/estimates/generate">Estimates List</Link>,
-  },
-];
+//   {
+//     key: '2',
+//     label: <Link href="/estimates/generate">Estimates List</Link>,
+//   },
+// ];
 
 const Tabs = () => {
   const pathname = usePathname();
@@ -47,226 +47,300 @@ const Tabs = () => {
         >
           Dashboard
         </li>
-        <li>
-          <Dropdown
-            menu={{
-              items: [
-                {
-                  key: 'bid-owner',
-                  label: <Link href="/bid/owner">Owner</Link>,
-                },
-                {
-                  key: 'bid-contractor',
-                  label: <Link href="/bid/contractor">Contractor</Link>,
-                },
-                {
-                  key: 'bid-sub-contractor',
-                  label: <Link href="/bid/sub-contractor">Sub Contractor</Link>,
-                },
-              ],
-              selectable: true,
-            }}
-          >
-            <Space
+        {
+          planFeatureOptions.map(feature => {
+            if (feature.options) {
+              return <li key={new Date().getTime()}>
+                <Dropdown
+                  menu={{
+                    items: feature.options.map(option => {
+                      return {
+                        key: option.value,
+                        label: <Link href={option.value}>{option.label}</Link>,
+                      }
+                    }),
+                    selectable: true,
+                  }}
+                >
+                  <Space
+                    className={twMerge(
+                      clsx(
+                        `${quaternaryHeading} text-steelGray
+                        flex items-stretch justify-center py-2 
+                         cursor-pointer
+                        `,
+                        pathname.includes('/bid'.split('/')[1]) && tabsStyle.active
+                      )
+                    )}
+                  >
+                    {feature.title}
+                    <DownOutlined />
+                  </Space>
+                </Dropdown>
+              </li>
+            }
+            return <li
+              key={new Date().getTime()}
               className={twMerge(
                 clsx(
                   `${quaternaryHeading} text-steelGray
-                    flex items-stretch justify-center py-2 
-                     cursor-pointer
-                    `,
-                  pathname.includes('/bid'.split('/')[1]) && tabsStyle.active
+                  flex items-stretch justify-center py-2 
+                   cursor-pointer
+                  `,
+                  pathname.includes(feature.value) && tabsStyle.active
                 )
               )}
+              onClick={() => router.push(feature.value)}
             >
-              Bid Management
-              <DownOutlined />
-            </Space>
-          </Dropdown>
-        </li>
-        <li>
-          <Dropdown
-            menu={{
-              items: [
-                {
-                  key: 'crm-clients',
-                  label: <Link href="/clients">Clients</Link>,
-                },
-                {
-                  key: 'crm-subcontractor',
-                  label: <Link href="/subcontractor">Sub Contractors</Link>,
-                },
-                {
-                  key: 'crm-partners',
-                  label: <Link href="/partners">Partners</Link>,
-                },
-              ],
-              selectable: true,
-            }}
-          >
-            <Space
-              className={twMerge(
-                clsx(
-                  `${quaternaryHeading} text-steelGray
-                    flex items-stretch justify-center py-2 
-                     cursor-pointer
-                    `,
-                  (pathname.includes('/clients') ||
-                    pathname.includes('/subcontractor') ||
-                    pathname.includes('/partners')) &&
-                    tabsStyle.active
-                )
-              )}
-            >
-              CRM
-              <DownOutlined />
-            </Space>
-          </Dropdown>
-        </li>
-        <li>
-          <Dropdown
-            menu={{
-              items: [
-                {
-                  key: 'manual',
-                  label: <Link href="/takeoff/manual">Manual</Link>,
-                },
-                {
-                  key: 'ai-takeoff',
-                  label: <Link href="/takeoff/ai">AI take off</Link>,
-                },
-              ],
-              selectable: true,
-            }}
-          >
-            <Space
-              className={twMerge(
-                clsx(
-                  `${quaternaryHeading} text-steelGray
-                    flex items-stretch justify-center py-2 
-                     cursor-pointer
-                    `,
-                  pathname.includes('/takeoff') && tabsStyle.active
-                )
-              )}
-            >
-              Quantity Takeoff
-              <DownOutlined />
-            </Space>
-          </Dropdown>
-        </li>
-        <li>
-          <Dropdown
-            menu={{
-              items,
-              selectable: true,
-            }}
-          >
-            <Space
-              className={twMerge(
-                clsx(
-                  `${quaternaryHeading} text-steelGray
-                    flex items-stretch justify-center py-2 
-                     cursor-pointer
-                    `,
-                  pathname.includes('/estimates'.split('/')[1]) &&
-                    tabsStyle.active
-                )
-              )}
-            >
-              Estimates
-              <DownOutlined />
-            </Space>
-          </Dropdown>
-        </li>
-        <li
-          className={twMerge(
-            clsx(
-              `${quaternaryHeading} text-steelGray
-                flex items-stretch justify-center py-2 
-                 cursor-pointer
-                `,
-              pathname.includes('/schedule') && tabsStyle.active
-            )
-          )}
-          onClick={() => router.push('/schedule')}
-        >
-          Schedule
-        </li>
-        <li>
-          <Dropdown
-            menu={{
-              items: [
-                {
-                  key: 'standard-invoicing',
-                  label: (
-                    <Link href="/invoices/standard-invoicing">
-                      Standard Invoicing
-                    </Link>
-                  ),
-                },
-                {
-                  key: 'aia-invoicing',
-                  label: (
-                    <Link href="/invoices/aia-invoicing">AIA Invoicing</Link>
-                  ),
-                },
-                {
-                  key: 'financial-tools',
-                  label: <Link href="/finance">Financial Tools</Link>,
-                },
-              ],
-              selectable: true,
-            }}
-          >
-            <Space
-              className={twMerge(
-                clsx(
-                  `${quaternaryHeading} text-steelGray
-                    flex items-stretch justify-center py-2 
-                     cursor-pointer
-                    `,
-                  (pathname.includes('/finance') ||
-                    pathname.includes('/invoices')) &&
-                    tabsStyle.active
-                )
-              )}
-            >
-              Financial
-              <DownOutlined />
-            </Space>
-          </Dropdown>
-        </li>
-        <li
-          className={twMerge(
-            clsx(
-              `${quaternaryHeading} text-steelGray
-                flex items-stretch justify-center py-2 
-                 cursor-pointer
-                `,
-              pathname.includes('/meeting'.split('/')[1]) && tabsStyle.active
-            )
-          )}
-          onClick={() => router.push('/meeting')}
-        >
-          Meetings
-        </li>
-        <li
-          className={twMerge(
-            clsx(
-              `${quaternaryHeading} text-steelGray
-                flex items-stretch justify-center py-2 
-                 cursor-pointer
-                `,
-              pathname.includes('/networking'.split('/')[1]) && tabsStyle.active
-            )
-          )}
-          onClick={() => router.push('/networking')}
-        >
-          Networking
-        </li>
+              {feature.label}
+            </li>
+          })
+        }
+
       </ul>
     </div>
   );
+
+  // return (
+  //   <div className="md:flex block justify-between bg-[#F0E9FD] items-center px-16 xl:h-[67px] shadow-quinaryGentle">
+  //     <ul
+  //       className="list-none flex flex-wrap xl:gap-8 gap-3 text-sm font-medium text-center
+  //           text-gray-500 dark:text-gray-400 justify-center mb-0"
+  //     >
+  //       <li
+  //         className={twMerge(
+  //           clsx(
+  //             `${quaternaryHeading} text-steelGray
+  //               flex items-stretch justify-center py-2 
+  //                cursor-pointer
+  //               `,
+  //             pathname === '/dashboard' && tabsStyle.active
+  //           )
+  //         )}
+  //         onClick={() => router.push('/dashboard')}
+  //       >
+  //         Dashboard
+  //       </li>
+  //       <li>
+  //         <Dropdown
+  //           menu={{
+  //             items: [
+  //               {
+  //                 key: 'bid-owner',
+  //                 label: <Link href="/bid/owner">Owner</Link>,
+  //               },
+  //               {
+  //                 key: 'bid-contractor',
+  //                 label: <Link href="/bid/contractor">Contractor</Link>,
+  //               },
+  //               {
+  //                 key: 'bid-sub-contractor',
+  //                 label: <Link href="/bid/sub-contractor">Sub Contractor</Link>,
+  //               },
+  //             ],
+  //             selectable: true,
+  //           }}
+  //         >
+  //           <Space
+  //             className={twMerge(
+  //               clsx(
+  //                 `${quaternaryHeading} text-steelGray
+  //                   flex items-stretch justify-center py-2 
+  //                    cursor-pointer
+  //                   `,
+  //                 pathname.includes('/bid'.split('/')[1]) && tabsStyle.active
+  //               )
+  //             )}
+  //           >
+  //             Bid Management
+  //             <DownOutlined />
+  //           </Space>
+  //         </Dropdown>
+  //       </li>
+  //       <li>
+  //         <Dropdown
+  //           menu={{
+  //             items: [
+  //               {
+  //                 key: 'crm-clients',
+  //                 label: <Link href="/clients">Clients</Link>,
+  //               },
+  //               {
+  //                 key: 'crm-subcontractor',
+  //                 label: <Link href="/subcontractor">Sub Contractors</Link>,
+  //               },
+  //               {
+  //                 key: 'crm-partners',
+  //                 label: <Link href="/partners">Partners</Link>,
+  //               },
+  //             ],
+  //             selectable: true,
+  //           }}
+  //         >
+  //           <Space
+  //             className={twMerge(
+  //               clsx(
+  //                 `${quaternaryHeading} text-steelGray
+  //                   flex items-stretch justify-center py-2 
+  //                    cursor-pointer
+  //                   `,
+  //                 (pathname.includes('/clients') ||
+  //                   pathname.includes('/subcontractor') ||
+  //                   pathname.includes('/partners')) &&
+  //                   tabsStyle.active
+  //               )
+  //             )}
+  //           >
+  //             CRM
+  //             <DownOutlined />
+  //           </Space>
+  //         </Dropdown>
+  //       </li>
+  //       <li>
+  //         <Dropdown
+  //           menu={{
+  //             items: [
+  //               {
+  //                 key: 'manual',
+  //                 label: <Link href="/takeoff/manual">Manual</Link>,
+  //               },
+  //               {
+  //                 key: 'ai-takeoff',
+  //                 label: <Link href="/takeoff/ai">AI take off</Link>,
+  //               },
+  //             ],
+  //             selectable: true,
+  //           }}
+  //         >
+  //           <Space
+  //             className={twMerge(
+  //               clsx(
+  //                 `${quaternaryHeading} text-steelGray
+  //                   flex items-stretch justify-center py-2 
+  //                    cursor-pointer
+  //                   `,
+  //                 pathname.includes('/takeoff') && tabsStyle.active
+  //               )
+  //             )}
+  //           >
+  //             Quantity Takeoff
+  //             <DownOutlined />
+  //           </Space>
+  //         </Dropdown>
+  //       </li>
+  //       <li>
+  //         <Dropdown
+  //           menu={{
+  //             items,
+  //             selectable: true,
+  //           }}
+  //         >
+  //           <Space
+  //             className={twMerge(
+  //               clsx(
+  //                 `${quaternaryHeading} text-steelGray
+  //                   flex items-stretch justify-center py-2 
+  //                    cursor-pointer
+  //                   `,
+  //                 pathname.includes('/estimates'.split('/')[1]) &&
+  //                   tabsStyle.active
+  //               )
+  //             )}
+  //           >
+  //             Estimates
+  //             <DownOutlined />
+  //           </Space>
+  //         </Dropdown>
+  //       </li>
+  //       <li
+  //         className={twMerge(
+  //           clsx(
+  //             `${quaternaryHeading} text-steelGray
+  //               flex items-stretch justify-center py-2 
+  //                cursor-pointer
+  //               `,
+  //             pathname.includes('/schedule') && tabsStyle.active
+  //           )
+  //         )}
+  //         onClick={() => router.push('/schedule')}
+  //       >
+  //         Schedule
+  //       </li>
+  //       <li>
+  //         <Dropdown
+  //           menu={{
+  //             items: [
+  //               {
+  //                 key: 'standard-invoicing',
+  //                 label: (
+  //                   <Link href="/invoices/standard-invoicing">
+  //                     Standard Invoicing
+  //                   </Link>
+  //                 ),
+  //               },
+  //               {
+  //                 key: 'aia-invoicing',
+  //                 label: (
+  //                   <Link href="/invoices/aia-invoicing">AIA Invoicing</Link>
+  //                 ),
+  //               },
+  //               {
+  //                 key: 'financial-tools',
+  //                 label: <Link href="/finance">Financial Tools</Link>,
+  //               },
+  //             ],
+  //             selectable: true,
+  //           }}
+  //         >
+  //           <Space
+  //             className={twMerge(
+  //               clsx(
+  //                 `${quaternaryHeading} text-steelGray
+  //                   flex items-stretch justify-center py-2 
+  //                    cursor-pointer
+  //                   `,
+  //                 (pathname.includes('/finance') ||
+  //                   pathname.includes('/invoices')) &&
+  //                   tabsStyle.active
+  //               )
+  //             )}
+  //           >
+  //             Financial
+  //             <DownOutlined />
+  //           </Space>
+  //         </Dropdown>
+  //       </li>
+  //       <li
+  //         className={twMerge(
+  //           clsx(
+  //             `${quaternaryHeading} text-steelGray
+  //               flex items-stretch justify-center py-2 
+  //                cursor-pointer
+  //               `,
+  //             pathname.includes('/meeting'.split('/')[1]) && tabsStyle.active
+  //           )
+  //         )}
+  //         onClick={() => router.push('/meeting')}
+  //       >
+  //         Meetings
+  //       </li>
+  //       <li
+  //         className={twMerge(
+  //           clsx(
+  //             `${quaternaryHeading} text-steelGray
+  //               flex items-stretch justify-center py-2 
+  //                cursor-pointer
+  //               `,
+  //             pathname.includes('/networking'.split('/')[1]) && tabsStyle.active
+  //           )
+  //         )}
+  //         onClick={() => router.push('/networking')}
+  //       >
+  //         Networking
+  //       </li>
+  //     </ul>
+  //   </div>
+  // );
 };
 
 export default Tabs;
