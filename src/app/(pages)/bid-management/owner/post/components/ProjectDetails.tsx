1bffef443209f445c4489ddef73d5f45d0a32f1c
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-03-23 01:37:14 +0500

    post project project type multi select, project building use multi select

--- src/app/(pages)/bid-management/owner/post/components/ProjectDetails.tsx
+++ src/app/(pages)/bid-management/owner/post/components/ProjectDetails.tsx
@@ -18,113 +18,122 @@ export function PostProjectDetails({ children }: Props) {
 
       <div className=" mt-4 space-y-3">
         <div className="grid grid-cols-2 gap-4">
-          <SelectComponent
-            label="Project Type"
-            name="projectType"
-            placeholder="Project Type"
-            field={{
-              options: [
-                { label: 'Addition', value: 'Addition' },
-                { label: 'Demolition', value: 'Demolition' },
-                {
-                  label: 'New Construction with Site Work',
-                  value: 'New Construction with Site Work',
-                },
-                {
-                  label: 'New Construction no Site Work',
-                  value: 'New Construction no Site Work',
-                },
-                {
-                  label: 'Renovation/Remodel/Repair',
-                  value: 'Renovation/Remodel/Repair',
-                },
-                { label: 'Site Work Only', value: 'Site Work Only' },
-                { label: 'Tenant Build-Out', value: 'Tenant Build-Out' },
-              ],
-            }}
-          />
+          <div>
+            <SelectComponent
+              label="Project Type"
+              name="projectType"
+              placeholder="Project Type"
+              field={{
+                options: [
+                  { label: 'Addition', value: 'Addition' },
+                  { label: 'Demolition', value: 'Demolition' },
+                  {
+                    label: 'New Construction with Site Work',
+                    value: 'New Construction with Site Work',
+                  },
+                  {
+                    label: 'New Construction no Site Work',
+                    value: 'New Construction no Site Work',
+                  },
+                  {
+                    label: 'Renovation/Remodel/Repair',
+                    value: 'Renovation/Remodel/Repair',
+                  },
+                  { label: 'Site Work Only', value: 'Site Work Only' },
+                  { label: 'Tenant Build-Out', value: 'Tenant Build-Out' },
+                ],
+                mode: "multiple",
+                showSearch: true,
 
-          <SelectComponent
-            label="Project Building Use"
-            name="projectBuildingUse"
-            placeholder="Project Building Use"
-            field={{
-              options: [
-                { label: 'Arena/Stadium', value: 'Arena/Stadium' },
-                {
-                  label: 'Assisted Living Facility/Elder Care',
-                  value: 'Assisted Living Facility/Elder Care',
-                },
-                { label: 'Auto Dealership', value: 'Auto Dealership' },
-                { label: 'Bank/Financial', value: 'Bank/Financial' },
-                { label: 'Casino', value: 'Casino' },
-                {
-                  label: 'Clinic/Hospital/Medical',
-                  value: 'Clinic/Hospital/Medical',
-                },
-                { label: 'Clubhouse', value: 'Clubhouse' },
-                {
-                  label: 'Conference/Convention Center',
-                  value: 'Conference/Convention Center',
-                },
-                {
-                  label: 'Convenience Store/Gas Station',
-                  value: 'Convenience Store/Gas Station',
-                },
-                { label: 'Day Care Center', value: 'Day Care Center' },
-                { label: 'Fitness Center', value: 'Fitness Center' },
-                { label: 'Golf Course', value: 'Golf Course' },
-                { label: 'Grocery Store', value: 'Grocery Store' },
-                { label: 'Hotel/Motel', value: 'Hotel/Motel' },
-                { label: 'Laboratory', value: 'Laboratory' },
-                { label: 'Medical', value: 'Medical' },
-                { label: 'Mixed-Use', value: 'Mixed-Use' },
-                { label: 'Office', value: 'Office' },
-                { label: 'Other', value: 'Other' },
-                { label: 'Private School', value: 'Private School' },
-                { label: 'Religious/Funeral', value: 'Religious/Funeral' },
-                { label: 'Restaurant', value: 'Restaurant' },
-                { label: 'Retail Store', value: 'Retail Store' },
-                { label: 'Salon', value: 'Salon' },
-                {
-                  label: 'Service Station/Car Wash',
-                  value: 'Service Station/Car Wash',
-                },
-                { label: 'Shell Building', value: 'Shell Building' },
-                { label: 'Shopping Center', value: 'Shopping Center' },
-                { label: 'Storage Facility', value: 'Storage Facility' },
-                { label: 'Theater/Auditorium', value: 'Theater/Auditorium' },
-                {
-                  label: 'Warehouse/Distribution',
-                  value: 'Warehouse/Distribution',
-                },
-                { label: 'Bridges/Tunnels', value: 'Bridges/Tunnels' },
-                { label: 'Bus Station', value: 'Bus Station' },
-                { label: 'Canal', value: 'Canal' },
-                { label: 'Docks/Marina', value: 'Docks/Marina' },
-                { label: 'Other', value: 'Other' },
-                { label: 'Parking Garage', value: 'Parking Garage' },
-                { label: 'Pipeline', value: 'Pipeline' },
-                { label: 'Road/Highway', value: 'Road/Highway' },
-                { label: 'Seawall', value: 'Seawall' },
-                {
-                  label: 'Sidewalk/Parking Lot',
-                  value: 'Sidewalk/Parking Lot',
-                },
-                { label: 'Solid Waste Station', value: 'Solid Waste Station' },
-                { label: 'Train Station', value: 'Train Station' },
-                {
-                  label: 'Transportation Terminal',
-                  value: 'Transportation Terminal',
-                },
-                {
-                  label: 'Waste Water Treatment',
-                  value: 'Waste Water Treatment',
-                },
-                { label: 'Water/Sewer', value: 'Water/Sewer' },
-              ],
-            }}
-          />
+              }}
+            />
+          </div>
+
+          <div>
+            <SelectComponent
+              label="Project Building Use"
+              name="projectBuildingUse"
+              placeholder="Project Building Use"
+              field={{
+                options: [
+                  { label: 'Arena/Stadium', value: 'Arena/Stadium' },
+                  {
+                    label: 'Assisted Living Facility/Elder Care',
+                    value: 'Assisted Living Facility/Elder Care',
+                  },
+                  { label: 'Auto Dealership', value: 'Auto Dealership' },
+                  { label: 'Bank/Financial', value: 'Bank/Financial' },
+                  { label: 'Casino', value: 'Casino' },
+                  {
+                    label: 'Clinic/Hospital/Medical',
+                    value: 'Clinic/Hospital/Medical',
+                  },
+                  { label: 'Clubhouse', value: 'Clubhouse' },
+                  {
+                    label: 'Conference/Convention Center',
+                    value: 'Conference/Convention Center',
+                  },
+                  {
+                    label: 'Convenience Store/Gas Station',
+                    value: 'Convenience Store/Gas Station',
+                  },
+                  { label: 'Day Care Center', value: 'Day Care Center' },
+                  { label: 'Fitness Center', value: 'Fitness Center' },
+                  { label: 'Golf Course', value: 'Golf Course' },
+                  { label: 'Grocery Store', value: 'Grocery Store' },
+                  { label: 'Hotel/Motel', value: 'Hotel/Motel' },
+                  { label: 'Laboratory', value: 'Laboratory' },
+                  { label: 'Medical', value: 'Medical' },
+                  { label: 'Mixed-Use', value: 'Mixed-Use' },
+                  { label: 'Office', value: 'Office' },
+                  { label: 'Other', value: 'Other' },
+                  { label: 'Private School', value: 'Private School' },
+                  { label: 'Religious/Funeral', value: 'Religious/Funeral' },
+                  { label: 'Restaurant', value: 'Restaurant' },
+                  { label: 'Retail Store', value: 'Retail Store' },
+                  { label: 'Salon', value: 'Salon' },
+                  {
+                    label: 'Service Station/Car Wash',
+                    value: 'Service Station/Car Wash',
+                  },
+                  { label: 'Shell Building', value: 'Shell Building' },
+                  { label: 'Shopping Center', value: 'Shopping Center' },
+                  { label: 'Storage Facility', value: 'Storage Facility' },
+                  { label: 'Theater/Auditorium', value: 'Theater/Auditorium' },
+                  {
+                    label: 'Warehouse/Distribution',
+                    value: 'Warehouse/Distribution',
+                  },
+                  { label: 'Bridges/Tunnels', value: 'Bridges/Tunnels' },
+                  { label: 'Bus Station', value: 'Bus Station' },
+                  { label: 'Canal', value: 'Canal' },
+                  { label: 'Docks/Marina', value: 'Docks/Marina' },
+                  { label: 'Other', value: 'Other' },
+                  { label: 'Parking Garage', value: 'Parking Garage' },
+                  { label: 'Pipeline', value: 'Pipeline' },
+                  { label: 'Road/Highway', value: 'Road/Highway' },
+                  { label: 'Seawall', value: 'Seawall' },
+                  {
+                    label: 'Sidewalk/Parking Lot',
+                    value: 'Sidewalk/Parking Lot',
+                  },
+                  { label: 'Solid Waste Station', value: 'Solid Waste Station' },
+                  { label: 'Train Station', value: 'Train Station' },
+                  {
+                    label: 'Transportation Terminal',
+                    value: 'Transportation Terminal',
+                  },
+                  {
+                    label: 'Waste Water Treatment',
+                    value: 'Waste Water Treatment',
+                  },
+                  { label: 'Water/Sewer', value: 'Water/Sewer' },
+                ],
+                mode: "multiple",
+                showSearch: true,
+              }}
+            />
+          </div>
         </div>
 
         <SelectComponent
