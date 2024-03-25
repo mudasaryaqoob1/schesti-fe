7f2230e5d92423056584130444a1f5743c362493
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-03-25 23:10:09 +0500

    create trade interface and service

---
+++ src/app/interfaces/trade.interface.ts
@@ -0,0 +1,19 @@
+export interface ITrade {
+    _id: string
+    user: string
+    tradeCategoryId: ITradeCategory
+    name: string
+    createdAt: string
+    updatedAt: string
+    
+  }
+  
+  export interface ITradeCategory {
+    _id: string
+    user: string
+    name: string
+    createdAt: string
+    updatedAt: string
+    
+  }
+  
---
+++ src/app/services/trade.service.ts
@@ -0,0 +1,11 @@
+import { IResponseInterface } from "../interfaces/api-response.interface";
+import { ITrade } from "../interfaces/trade.interface";
+import { HttpService } from "./base.service";
+
+class TradeService extends HttpService {
+    private readonly prefix: string = 'api/trade';
+
+    httpGetAllTrades = ():Promise<IResponseInterface<{trades:ITrade[]}>> => this.get(`${this.prefix}/all`);
+}
+
+export const tradeService = new TradeService();
