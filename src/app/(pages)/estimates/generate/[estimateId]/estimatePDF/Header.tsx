20bd0d34a6eb22886a1ca36e53a59946c4e20a23
@azher <azher@renesistech.com>
2024-03-28 01:00:20 +0500

    pdf creation for estimate complete

---
+++ src/app/(pages)/estimates/generate/[estimateId]/estimatePDF/Header.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, View } from '@react-pdf/renderer';
+
+export const BG_COLOR = '#6f42c1';
+type Props = {
+  brandingColor?: string;
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 44,
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+  },
+});
+
+export function Header({ brandingColor }: Props) {
+  return (
+    <View
+      style={[
+        styles.header,
+        {
+          backgroundColor: brandingColor ? brandingColor : BG_COLOR,
+        },
+      ]}
+    ></View>
+  );
+}
