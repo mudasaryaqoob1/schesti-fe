c5ec4ca5f1b44e19f559b5bb965a54e12cd2a68a
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-04-01 21:25:48 +0500

    project view design team

--- src/app/(pages)/bid-management/owner/view/[id]/components/ProjectDesignTeam.tsx
+++ src/app/(pages)/bid-management/owner/view/[id]/components/ProjectDesignTeam.tsx
@@ -1,3 +1,97 @@
+import TertiaryHeading from "@/app/component/headings/tertiary";
+import { Dropdown, Table, type TableProps } from "antd";
+import Image from "next/image";
+
 export function ProjectDesignTeam() {
-  return <div>Project DesignTeam</div>;
+  const columns: TableProps['columns'] = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Role',
+      dataIndex: 'role',
+      key: 'role',
+    },
+    {
+      title: 'Company Name',
+      dataIndex: 'companyName',
+      key: 'companyName',
+    },
+    {
+      title: 'Location',
+      dataIndex: 'location',
+      key: 'location',
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phoneNumber',
+      key: 'phoneNumber',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render(v, record) {
+        return (
+          <Dropdown
+            menu={{
+              items: [
+                {
+                  key: 'edit',
+                  label: <p>Edit</p>,
+                },
+                {
+                  key: 'delete',
+                  label: <p>Delete</p>,
+                },
+              ],
+              onClick: ({ key }) => {
+                if (key === 'edit') {
+
+                }
+                if (key === 'delete') {
+
+                }
+              },
+            }}
+            placement="bottomRight"
+            trigger={['click']}
+          >
+            <Image
+              src={'/menuIcon.svg'}
+              alt="logo white icon"
+              width={20}
+              height={20}
+              className="active:scale-105 cursor-pointer"
+            />
+          </Dropdown>
+        );
+      },
+    },
+  ];
+  return <div className=" mt-6 mb-4 md:ms-[69px] md:me-[59px] mx-4  p-5 bg-white rounded-lg border shadow-lg">
+    <div>
+      <TertiaryHeading
+        title="Design Team"
+        className="text-[20px] leading-[30px]"
+      />
+    </div>
+
+    <div className="mt-5">
+      <Table
+        columns={columns}
+        bordered
+        dataSource={[
+          { name: "John Doe", role: "Architect", companyName: "ABC", location: "New York", phoneNumber: "1234567890", email: "johnDoe@gmail.com" },
+          { name: "John Doe", role: "Architect", companyName: "ABC", location: "New York", phoneNumber: "1234567890", email: "john@gmail.com" },
+        ]}
+      />
+    </div>
+  </div>
 }
