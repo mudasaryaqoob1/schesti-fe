407e0d781b9d22fbc5225265896781a920017e40
Fahad Ali <85811874+fahadali503@users.noreply.github.com>
2024-04-18 19:43:55 +0500

    get invoice by id

--- src/app/(pages)/financial/aia-invoicing/invoice/[id]/page.tsx
+++ src/app/(pages)/financial/aia-invoicing/invoice/[id]/page.tsx
@@ -1,21 +1,44 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import { useSelector } from 'react-redux';
 import { NoInvoiceFound } from './components/NoInvoiceFound';
-import { selectClientInvoices } from '@/redux/client-invoices/client-invoice.selector';
 import { PhaseComponent } from './components';
 import { withAuth } from '@/app/hoc/withAuth';
+import { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
+import { clientInvoiceService } from '@/app/services/client-invoices.service';
+import { useEffect, useState } from 'react';
+import { IClientInvoice } from '@/app/interfaces/client-invoice.interface';
+import { Skeleton } from 'antd';
 
-function CreateClientInvoicePage() {
+function CreateNextPayableInvoicePage() {
+  const [loading, setLoading] = useState(false);
   const params = useParams<{ id: string }>();
-  // all parent invoices
-  const allInvoices = useSelector(selectClientInvoices);
+  const [parentInvoice, setParentInvoice] = useState<IClientInvoice | null>(null);
 
-  const parentInvoice = allInvoices?.find(
-    (invoice) => invoice._id === params.id
-  );
+  useEffect(() => {
+    getParentInvoice();
+  }, [params.id])
 
+  async function getParentInvoice() {
+    if (params.id) {
+      setLoading(true);
+      try {
+        const response = await clientInvoiceService.httpGetParentInvoiceById(params.id);
+        if (response.data) {
+          setParentInvoice(response.data.invoice);
+        }
+      } catch (error) {
+        const err = error as AxiosError<{ message: string }>;
+        toast.error(err.response?.data.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+  }
+  if (loading) {
+    return <Skeleton />;
+  }
   if (!parentInvoice) {
     return <NoInvoiceFound />;
   }
@@ -24,4 +47,4 @@ function CreateClientInvoicePage() {
 }
 
 
-export default withAuth(CreateClientInvoicePage);
+export default withAuth(CreateNextPayableInvoicePage);
