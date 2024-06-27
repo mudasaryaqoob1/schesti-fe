3609f6e5aa84f7ecbcbf4d54e13556ea28336dda
Fahad Ali <fahada094@gmail.com>
2024-06-27 20:47:09 +0500

    add ellipses in auth wrapper

--- src/app/component/welcomeLayout/index.tsx
+++ src/app/component/welcomeLayout/index.tsx
@@ -1,4 +1,5 @@
 import Welcome from '@/app/(pages)/(auth)/welcome';
+import Image from 'next/image';
 
 type Props = {
   children: React.ReactNode;
@@ -10,8 +11,37 @@ const WelcomeWrapper = ({ children }: Props) => {
       <div className="px-14 py-7">{children}</div>
       <div
         className="items-center relative  bg-schestiLightPrimary
-       hidden md:flex h-full"
+       hidden md:flex h-full overflow-x-hidden"
       >
+        <Image
+          src={"/ellipse.png"}
+          alt="ellipse"
+          width={411.57}
+          height={414.68}
+          className='absolute -top-4 -right-40'
+        />
+        <Image
+          src={"/ellipse.png"}
+          alt="ellipse"
+          width={564.35}
+          height={567.46}
+          className='absolute -top-4 -right-36'
+        />
+        <Image
+          src={"/ellipse.png"}
+          alt="ellipse"
+          width={742.07}
+          height={745.19}
+          className='absolute -top-4 -right-32'
+        />
+
+        <Image
+          src={"/ellipse.png"}
+          alt="ellipse"
+          width={800.74}
+          height={800}
+          className='absolute -top-4 -right-16'
+        />
         <Welcome />
       </div>
     </div>
