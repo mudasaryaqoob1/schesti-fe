7d60094ea02477ec0ecda5bd8ed18d1679542625
Fahad Ali <fahada094@gmail.com>
2024-06-27 20:00:55 +0500

    change auth wrapper bg

--- src/app/component/welcomeLayout/index.tsx
+++ src/app/component/welcomeLayout/index.tsx
@@ -9,7 +9,7 @@ const WelcomeWrapper = ({ children }: Props) => {
     <div className="h-[100vh] bg-white relative grid grid-cols-1 md:grid-cols-2 overflow-y-auto">
       <div className="px-14 py-7">{children}</div>
       <div
-        className="items-center relative  bg-cloudWhite
+        className="items-center relative  bg-schestiLightPrimary
        hidden md:flex h-full"
       >
         <Welcome />
