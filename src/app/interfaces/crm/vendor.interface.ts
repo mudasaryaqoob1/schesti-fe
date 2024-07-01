dabf766f6863c8994eff283544fd413659079f7d
Fahad Ali <fahada094@gmail.com>
2024-07-01 18:11:29 +0500

    crm vendor interface

---
+++ src/app/interfaces/crm/vendor.interface.ts
@@ -0,0 +1,11 @@
+export interface ICrmVendor {
+    firstName: string
+    lastName: string
+    companyName: string
+    email: string
+    phone: string
+    status: boolean
+    address: string
+    secondAddress: string
+    _id: string
+}
