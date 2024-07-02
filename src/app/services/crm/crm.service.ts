1c18f78b11470f7f0fe2e9b68ddf4db8690c6123
Fahad Ali <fahada094@gmail.com>
2024-07-02 18:03:56 +0500

    update crm service create and update

--- src/app/services/crm/crm.service.ts
+++ src/app/services/crm/crm.service.ts
@@ -1,6 +1,6 @@
 import { IResponseInterface } from "@/app/interfaces/api-response.interface";
 import { HttpService } from "../base.service";
-import { CommonCrmType, CrmModuleType, CrmType } from "@/app/interfaces/crm/crm.interface";
+import { CommonCrmType, CrmModuleType, CrmType, ICrmSubcontractorModule } from "@/app/interfaces/crm/crm.interface";
 
 
 type CreateCrmItemType = {
@@ -14,14 +14,18 @@ type CreateCrmItemType = {
     module: CrmModuleType;
 };
 
+type CreateCrmSubcontractorType = Omit<ICrmSubcontractorModule, "_id" | "createdAt" | "updatedAt" | "associatedCompany">
+
 type QueryParams = {
     module: CrmModuleType
 }
 
+type CreateCrmItem = CreateCrmSubcontractorType | CreateCrmItemType;
+
 class CrmService extends HttpService {
     private endPoint = 'api/crm';
 
-    httpCreate = (data: CreateCrmItemType): Promise<IResponseInterface<CrmType>> => this.post(this.endPoint, data);
+    httpCreate = (data: CreateCrmItem): Promise<IResponseInterface<CrmType>> => this.post(this.endPoint, data);
 
     httpCreateMany = (data: CommonCrmType[], module: CrmModuleType): Promise<IResponseInterface<{
         duplicates: CommonCrmType[];
@@ -32,7 +36,7 @@ class CrmService extends HttpService {
 
     httpGetItemById = (id: string): Promise<IResponseInterface<CrmType>> => this.get(`${this.endPoint}/${id}`);
 
-    httpfindByIdAndUpdate = (id: string, data: CreateCrmItemType): Promise<IResponseInterface<CrmType>> => this.put(`${this.endPoint}/${id}`, data);
+    httpfindByIdAndUpdate = (id: string, data: CreateCrmItem): Promise<IResponseInterface<CrmType>> => this.put(`${this.endPoint}/${id}`, data);
 
     httpfindByIdAndDelete = (id: string): Promise<IResponseInterface<CrmType>> => this.put(`${this.endPoint}/${id}`);
 
