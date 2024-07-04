d109960cff2de69bd32ffa3af6da0e9ce16a44a5
Fahad Ali <fahada094@gmail.com>
2024-07-04 23:55:37 +0500

    fix settings/plans toggle and switch btn

--- src/app/component/plans/switchbtn/index.tsx
+++ src/app/component/plans/switchbtn/index.tsx
@@ -4,18 +4,17 @@ interface SwitchBtnProps {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   isDuration: string;
 }
-const Index: React.FC<SwitchBtnProps> = ({ isDuration, onChange }) => {
+const SwitchBtn: React.FC<SwitchBtnProps> = ({ isDuration, onChange }) => {
   return (
     <label
       htmlFor="durationToggle"
       className="inline-flex gap-6 items-center space-x-4 cursor-pointer dark:text-gray-100"
     >
       <span
-        className={`${tertiaryHeading} font-normal ${
-          isDuration == 'monthly'
+        className={`${tertiaryHeading} font-normal ${isDuration == 'monthly'
             ? 'text-black font-bold'
             : 'font-normal text-gray-400'
-        }`}
+          }`}
       >
         Monthly
       </span>
@@ -33,11 +32,10 @@ const Index: React.FC<SwitchBtnProps> = ({ isDuration, onChange }) => {
         <div className="absolute inset-y-0 left-0 w-6 h-6 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto bg-snowWhite"></div>
       </span>
       <span
-        className={`${tertiaryHeading} font-normal ${
-          isDuration == 'yearly'
+        className={`${tertiaryHeading} font-normal ${isDuration == 'yearly'
             ? 'text-black font-bold'
             : 'font-normal text-gray-400'
-        }`}
+          }`}
       >
         Yearly
       </span>
@@ -45,4 +43,4 @@ const Index: React.FC<SwitchBtnProps> = ({ isDuration, onChange }) => {
   );
 };
 
-export default Index;
+export default SwitchBtn;
