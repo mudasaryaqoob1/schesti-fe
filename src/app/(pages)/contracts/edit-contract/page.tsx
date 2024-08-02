aced0fb80edbdd3cdabd8be9b2f86df2aa811e01
Fahad Ali <fahada094@gmail.com>
2024-08-02 16:46:39 +0500

    show sender and receiver receipts

--- src/app/(pages)/contracts/edit-contract/page.tsx
+++ src/app/(pages)/contracts/edit-contract/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 import NoData from '@/app/component/noData';
 import { withAuth } from '@/app/hoc/withAuth';
-import { ICrmContract } from '@/app/interfaces/crm/crm-contract.interface';
+import { ContractPartyType, ICrmContract } from '@/app/interfaces/crm/crm-contract.interface';
 import crmContractService from '@/app/services/crm/crm-contract.service';
 import { Routes } from '@/app/utils/plans.utils';
 import { Skeleton } from 'antd';
@@ -16,12 +16,17 @@ import WhiteButton from '@/app/component/customButton/white';
 import SenaryHeading from '@/app/component/headings/senaryHeading';
 import { ToolState } from '../types';
 import { useRouterHook } from '@/app/hooks/useRouterHook';
+import { SelectComponent } from '@/app/component/customSelect/Select.component';
 
 function EditContractDocumentPage() {
   const [contract, setContract] = useState<ICrmContract | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const searchParams = useSearchParams();
   const [tools, setTools] = useState<ToolState[]>([]);
+  const [senderRecipets, setSenderRecipets] = useState<ContractPartyType[]>([]);
+  const [receiverRecipets, setReceiverRecipets] = useState<ContractPartyType[]>([]);
+  const [selectedSenderReceipt, setSelectedSenderReceipt] = useState<string>('');
+  const [selectedReceiverReceipt, setSelectedReceiverReceipt] = useState<string>('');
   const [isSending, setIsSending] = useState(false);
   const router = useRouterHook();
 
@@ -39,6 +44,8 @@ function EditContractDocumentPage() {
       const response = await crmContractService.httpFindContractById(id);
       if (response.data) {
         setContract(response.data);
+        setSenderRecipets(response.data.senders);
+        setReceiverRecipets(response.data.receivers);
       }
     } catch (error) {
       const err = error as AxiosError<{ message: string }>;
@@ -108,6 +115,41 @@ function EditContractDocumentPage() {
             isLoading={isSending}
           />
         </div>
+      </div>
+      <div className='flex items-center space-x-2'>
+        <SelectComponent
+          label='Sender Receipts'
+          name='receipts'
+          placeholder='Select sender receipt'
+          field={{
+            value: selectedSenderReceipt ? selectedSenderReceipt : undefined,
+            options: senderRecipets.map((receipt) => ({
+              label: `${receipt.name}-${receipt.companyName}`,
+              value: receipt.email
+            })),
+            onChange: (value) => {
+              setSelectedSenderReceipt(value);
+              setSelectedReceiverReceipt('');
+            }
+          }}
+        />
+
+        <SelectComponent
+          label='Receiver Receipts'
+          name='receipts'
+          placeholder='Select receiver receipt'
+          field={{
+            value: selectedReceiverReceipt ? selectedReceiverReceipt : undefined,
+            options: receiverRecipets.map((receipt) => ({
+              label: `${receipt.name}-${receipt.companyName}`,
+              value: receipt.email
+            })),
+            onChange: (value) => {
+              setSelectedReceiverReceipt(value);
+              setSelectedSenderReceipt('');
+            }
+          }}
+        />
       </div>
       <ContractPdf
         mode="edit-fields"
