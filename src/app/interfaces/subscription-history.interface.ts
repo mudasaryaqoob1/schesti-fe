ce47c9b0baae42b85840ef99f1c2ad34051890c8
Fahad Ali <fahada094@gmail.com>
2024-08-24 18:11:18 +0500

    update user

--- src/app/interfaces/subscription-history.interface.ts
+++ src/app/interfaces/subscription-history.interface.ts
@@ -1,33 +1,19 @@
 import { IPricingPlan } from "./pricing-plan.interface";
 import { IUserInterface } from "./user.interface";
 
-type CanceledSubscriptionHistroy = {
-  status: 'canceled';
-  canceledAt: Date;
-};
 
-type ExpiredSubscriptionHistroy = {
-  status: 'expired';
-  expiredAt: Date;
-};
-
-type ActiveSubscriptionHistroy = {
-  status: 'active';
-  currentPeriodStart: Date;
-  currentPeriodEnd: Date;
-};
-
-export type ISubriptionHistory = (
-  | CanceledSubscriptionHistroy
-  | ExpiredSubscriptionHistroy
-  | ActiveSubscriptionHistroy
-) & {
+export type ISubriptionHistory = {
   customerId: string;
+  status: "canceled" | "expired" | "active";
   planId: string | IPricingPlan;
   user: string | IUserInterface;
   subscriptionId: string;
   paymentMethod: string;
   amount: number;
   additionalPeriodEnd: Date;
+  expiredAt: Date;
+  canceledAt: Date;
+  currentPeriodStart: Date;
+  currentPeriodEnd: Date;
 };
 
