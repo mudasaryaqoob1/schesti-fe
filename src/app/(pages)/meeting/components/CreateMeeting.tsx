14e57b66d9aec92392d266aa08f9257dbba944c0
Fahad Ali <fahada094@gmail.com>
2024-09-23 16:28:30 +0500

    recurrence ui

--- src/app/(pages)/meeting/components/CreateMeeting.tsx
+++ src/app/(pages)/meeting/components/CreateMeeting.tsx
@@ -271,6 +271,32 @@ export function CreateMeeting({
                   }}
                 />
 
+                {formik.values.recurrence?.frequency === 'weekly' ? <>
+                  <SelectComponent
+                    label='Select Days'
+                    name='recurrence.days'
+                    placeholder='Select Days'
+                    field={{
+                      value: formik.values.recurrence?.days,
+                      options: [
+                        { label: "Monday", value: 1 },
+                        { label: "Tuesday", value: 2 },
+                        { label: "Wednesday", value: 3 },
+                        { label: "Thursday", value: 4 },
+                        { label: "Friday", value: 5 },
+                        { label: "Saturday", value: 6 },
+                        { label: "Sunday", value: 0 }
+                      ],
+                      mode: "multiple",
+
+                      onChange: (value) => {
+                        formik.setFieldValue('recurrence.days', value);
+                      },
+                      onBlur: formik.handleBlur,
+                    }}
+                  />
+                </> : null}
+
                 {formik.values.recurrence?.frequency === "custom" ? <>
                   <DateInputComponent
                     label='Select Date'
