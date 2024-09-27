16afb7894d69a7259e7f6081662ac10bd4c625f6
Fahad Ali <fahada094@gmail.com>
2024-09-27 20:36:45 +0500

    refactor estimator listing sale persson and esstimator name

--- src/app/(pages)/estimates/generate/components/takeoff/index.tsx
+++ src/app/(pages)/estimates/generate/components/takeoff/index.tsx
@@ -94,9 +94,8 @@ const TakeOff = ({ setPrevNext, pevNext }: Props) => {
           </div>
         </div>
         <div
-          className={`mt-4 md:grid-cols-4 md:grid-rows-2 gap-y-6 ${
-            showEstimateDetails ? 'grid' : 'hidden'
-          }`}
+          className={`mt-4 md:grid-cols-4 md:grid-rows-2 gap-y-6 ${showEstimateDetails ? 'grid' : 'hidden'
+            }`}
         >
           <div>
             <MinDesc
@@ -169,7 +168,7 @@ const TakeOff = ({ setPrevNext, pevNext }: Props) => {
               className="font-popin font-normal text-base text-lightyGray"
             />
             <MinDesc
-              title={`${estimateRequestDetail?.salePerson?.firstName} ${estimateRequestDetail?.salePerson?.lastName}`}
+              title={estimateRequestDetail?.salePerson ? `${estimateRequestDetail?.salePerson?.firstName} ${estimateRequestDetail?.salePerson?.lastName}` : ""}
               className="font-popin text-base text-midnightBlue mt-2"
             />
           </div>
@@ -180,7 +179,7 @@ const TakeOff = ({ setPrevNext, pevNext }: Props) => {
               className="font-popin  font-normal text-base text-lightyGray"
             />
             <MinDesc
-              title={`${estimateRequestDetail?.estimator?.firstName} ${estimateRequestDetail?.estimator?.lastName}`}
+              title={estimateRequestDetail?.estimator ? `${estimateRequestDetail?.estimator?.firstName} ${estimateRequestDetail?.estimator?.lastName}` : ""}
               className="font-popin text-base text-midnightBlue mt-2"
             />
           </div>
