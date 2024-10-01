27917360d5dbf4ac0ec0740a6ceeed9ed2c353a3
azher-saeed <azher.saeed@hutfin.com>
2024-10-01 13:45:51 +0500

    typescripte disabled for download link

--- src/app/(pages)/bid-management/projects/components/ExportProjectActivityAndStatusTracking.tsx
+++ src/app/(pages)/bid-management/projects/components/ExportProjectActivityAndStatusTracking.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 'use client';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import { IBidActivity } from '@/app/interfaces/bid-management/bid-management.interface';
